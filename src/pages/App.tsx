45cbe838e1833b5fab2ae324bc416b91457838ac
Pedro Benjamin <benjamin23ph@gmail.com>
2024-08-12 20:51:56 -0300

    first-commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import style from './App.module.scss';
+import Cronometro  from '../components/Cronometro';
+import { useState } from 'react';
+import { ITarefa } from '../types/tarefa';
+
+function App() {
+  
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>()
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+  }
+
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas}/>
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro selecionado={selecionado} />
+    </div>
+  );
+}
+
+export default App;
